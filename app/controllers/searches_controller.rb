5bffba3136e90180207b2fbca7d318b114a2a9b2
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-10 15:17:26 +0000

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,6 +8,7 @@ class SearchesController < ApplicationController
       req.params['v'] = '20160201'
     end
     @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
+    binding.pry
   end
 
   def foursquare
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   def create
     resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
       req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
-      req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
+      req.params['client_secret'] = ENV['FOURSQUARE_CLIENT_SECRET']
       req.params['grant_type'] = 'authorization_code'
-      req.params['redirect_uri'] = "http://localhost:3000/auth"
+      req.params['redirect_uri'] = "http://67.205.165.109:34159/auth"
       req.params['code'] = params[:code]
     end
 
